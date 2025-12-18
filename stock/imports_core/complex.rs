3a592b774f8c6475530af48f1c9cf4c6625e4c50
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2025-12-18 17:35:00 -0500

    Add crimp driver code

--- stock/imports_core/complex.rs
+++
@@ -1,3 +0,0 @@
-// Import handler implementations
-pub fn host_double_fn(param: i32) -> i32 { param * 2 }
-pub fn host_complex_fn(p1: i32, p2: i64) -> (i32, i64, f32) { ( (p1 as f32).sqrt() as i32, (p1 * p1) as i64 * p2, 8.66 ) }
